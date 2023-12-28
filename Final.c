a78107b8e63e0f6741b808df1d017b030e148a2a
John_6670 <mayn2005m@gmail.com>
2023-12-28 17:24:58 +0330

    first

--- Final.c
+++ Final.c
@@ -1,10 +1,60 @@
 #include <stdio.h>
 #include <conio.h>
 #include <time.h>
+#include <stdlib.h>
 
 void mainMenu();
+void signIn();
+void logIn();
 
 void main()
+{
+    int choice;
+
+    while (1)
+    {
+        mainMenu();
+        scanf("%d", &choice);
+        system("cls");
+
+        switch (choice)
+        {
+        case 1:
+            signIn();
+            break;
+
+        case 2:
+            logIn();
+            break;
+
+        case 3:
+            exit(0);
+            break;
+        
+        default:
+            printf("Invalid input.");
+            continue;
+            break;
+        }
+    }
+}
+
+void mainMenu()
+{
+    printf("\n\n");
+    printf("1. Sign Up\n");
+    printf("2. Log In\n");
+    printf("3. Exit\n\n");
+
+    printf("Choose an action from above menu: ");
+}
+
+void signIn()
+{
+    printf
+}
+
+void logIn()
 {
 
 }
